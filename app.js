919d568bf51d029e604c1e0986ac2fc7db375c97
erik <erik.e.wilson@gmail.com>
2012-10-13 14:15:10 -0700

    working lines

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ var app = express()
 
 server.listen(8090);
 
-console.log("opening sockets...");
 io.sockets.on('connection', function (socket) {
 
-  console.log(socket);
-  socket.emit('news', { hello: 'world' });
-
   socket.on('line', function (color,x1,y1,x2,y2) {
-    console.log(color,x1,y1,x2,y2);
-    socket.broadcast.emit(color,x1,y1,x2,y2);
+    console.log("line info: ",color,x1,y1,x2,y2);
+    socket.broadcast.emit("line",color,x1,y1,x2,y2);
   });
 });
