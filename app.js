2e675985255cec1b4e381fafd2c88a00f955c51b
Rob Richardson <robrich@robrich.org>
2012-10-27 01:34:21 -0700

    Server rooms / sessions: now multi-user

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 /*global node:true */
 "use strict";
 
+//
+// The server
+//
+
 var express = require("express"),
 	io = require("socket.io"),
 	http = require("http");
@@ -22,36 +26,80 @@ sio.configure(function () {
 	sio.set('log level', 1);
 });
 
-server.listen(8090);
+var port = process.env.PORT || 8090;
+server.listen(port, function () {
+	console.log("Listening on "+port);
+});
 
 // Store state so we can broadcast it to clients that connect late or reconnect
-var drawing = [];
-var img = null;
+var sessions = {};
 
 // -server is "from server to client"
 // -client is "from client to server"
 sio.sockets.on('connection', function (socket) {
-	if (img) {
-		img.drawing = drawing;
-		socket.emit("image-server", img);
-		img.drawing = undefined;
-	} else {
-		socket.emit("drawing-server", drawing);
-	}
+	var socketSession = null;
+	
+	var broadcast = function (event, data) {
+		sio.sockets.in(socketSession.room).emit(event, data)
+	};
+
+	socket.on('connect-client', function (data) {
+		var room = 's-'+data;
+		if (room === 's-' || room === 's-[object Object]') {
+			socket.disconnect(); // You fail
+			return;
+		}
+
+		// Create the session if it doesn't exist
+		if (!Object.prototype.hasOwnProperty.call(sessions, room)) {
+			sessions[room] = {
+				drawing: [],
+				//sockets: [],
+				img: null,
+				room: room
+			};
+		}
+		socketSession = sessions[room];
+		socket.join(room);
+		//socketSession.sockets.push(socket);
+
+		// Replay old data to the user
+		if (socketSession.img) {
+			socketSession.img.drawing = socketSession.drawing;
+			socket.emit("image-server", socketSession.img);
+			socketSession.img.drawing = undefined;
+		} else if (socketSession.drawing.length > 0) {
+			socket.emit("drawing-server", socketSession.drawing);
+		}
+	});
+
 	socket.on('line-client', function (data) {
-		drawing.push(data);
-		socket.broadcast.emit("line-server",data);
+		if (!socketSession) {
+			socket.disconnect();
+			return;
+		}
+		socketSession.drawing.push(data);
+		broadcast("line-server",data);
 	});
 
 	socket.on('image-client', function (data) {
-		img = data;
-		drawing = []; // Image overwrites it
-		socket.broadcast.emit("image-server",data);
+		if (!socketSession) {
+			socket.disconnect();
+			return;
+		}
+		socketSession.img = data;
+		socketSession.drawing = []; // Image overwrites it
+		broadcast("image-server",data);
 	});
 
 	socket.on('clear-client', function (data) {
-		drawing = [];
-		img = null;
-		socket.broadcast.emit("clear-server",data);
+		if (!socketSession) {
+			socket.disconnect();
+			return;
+		}
+		socketSession.drawing = [];
+		socketSession.img = null;
+		broadcast("clear-server",data);
 	});
+
 });
